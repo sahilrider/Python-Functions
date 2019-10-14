69c31ca3a0d32990927df669fffce8d8ddc6a7a8
Sagar2366 <sagarutekar2366@gmail.com>
2019-10-14 22:46:02 +0530

    Adding asc and desc order sorting example
    Adding asc and desc order sorting example

---
+++ Basics/sortListPython.py
@@ -0,0 +1,10 @@
+numbers = int(input().split())
+  
+# Sorting list of Integers in ascending order
+numbers.sort() 
+print(numbers) 
+
+
+# Sorting list of Integers in descending order
+numbers.sort(reverse=True)
+print(numbers)
